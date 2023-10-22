06595f59e80e9489f4ad08137c455008523c45a9
Romain Benard <romain.benard@swile.co>
2023-10-22 13:10:04 +0200

    feat: Input component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+import Input from '@/components/form/Input'
 import LogoWithBaseline from '@/components/ui/LogoWithBaseline'
 import { Colors } from '@/types/Colors.d'
 
@@ -5,6 +6,10 @@ const LoginPage = () => {
   return (
     <main className="w-screen h-screen bg-secondary-500 flex flex-col items-center justify-center">
       <LogoWithBaseline color={Colors.WHITE} />
+
+      <form className="mt-8">
+        <Input type="text" />
+      </form>
     </main>
   )
 }
---
+++ src/components/form/Input.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import cn from '@/utils/cn'
+import {
+  ChangeEventHandler,
+  FC,
+  InputHTMLAttributes,
+  forwardRef,
+  useEffect,
+  useState,
+} from 'react'
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, Props>(function Input(
+  { className, type, placeholder, ...props },
+  ref
+) {
+  const [value, setValue] = useState<typeof props.value>(props.value ?? '')
+
+  useEffect(() => {
+    setValue(props.value ?? '')
+  }, [props.value])
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
+    setValue(e.target.value)
+    if (props.onChange) props.onChange(e)
+  }
+
+  return (
+    <input
+      {...props}
+      ref={ref}
+      type={type}
+      className={cn(
+        'bg-white rounded-lg p-2 text-secondary-600 placeholder-secondary-200',
+        className
+      )}
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+    />
+  )
+})
+
+export default Input
