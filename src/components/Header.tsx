31b2a648aadd1166e89920fcc708f7b202726984
Romain Benard <romain.benard@swile.co>
2023-11-11 17:26:39 +0100

    style: unfixed header + adjust main layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Header: FC = () => {
   return (
-    <header className="flex justify-between items-center p-6 h-20 fixed top-0 w-full">
+    <header className="flex justify-between items-center p-6 h-20 w-full">
       <Logo height={2.5 * 16} />
       <Link className="text-primary-500" href="/about-us">
         About us
