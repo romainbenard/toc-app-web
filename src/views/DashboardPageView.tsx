54de929900f333a8e905633396fb7a5f58a7ba7c
Romain Benard <romain.benard@swile.co>
2023-11-12 20:54:31 +0100

    feat: add button to test

--- src/views/DashboardPageView.tsx
+++ src/views/DashboardPageView.tsx
@@ -1,16 +1,8 @@
 'use client'
 
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import Input from '@/components/ui/Input'
-import Label from '@/components/ui/Label'
-import Button from '@/components/ui/Button'
-import config from '@/config'
 import { useSession } from 'next-auth/react'
-import { zodResolver } from '@hookform/resolvers/zod'
 import MainLayout from '@/components/Layout/MainLayout'
-
-const { server } = config
+import Link from 'next/link'
 
 const DashboardPageView = () => {
   const { data: session } = useSession()
@@ -21,6 +13,12 @@ const DashboardPageView = () => {
         <h1 className="text-secondary-500">
           Hello {session?.user?.name || 'you'} !
         </h1>
+
+        <Link href="/ocd/new">
+          <div className="flex justify-center items-center h-12 w-12 rounded-[50%] bg-primary-500 text-white font-semibold text-4xl">
+            +
+          </div>
+        </Link>
       </main>
     </MainLayout>
   )
