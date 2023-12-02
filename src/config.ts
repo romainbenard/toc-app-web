0b86b51816ff47eccf6b23043df1ba9e4a403036
romainbenard <90315686+romainbenard@users.noreply.github.com>
2023-12-02 18:32:13 +0100

    refactor: use vercel env variable (#14)

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 const config = {
   env: process.env.NODE_ENV,
-  appUrl: process.env.NEXT_PUBLIC_APP_URL,
+  appUrl:
+    process.env.NEXT_PUBLIC_APP_URL ??
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
   server: {
     url: process.env.NEXT_PUBLIC_SERVER_URL,
   },
