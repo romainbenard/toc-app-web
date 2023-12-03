41c6cb341cd34fa321c06c1c2012bf70178c93d6
romainbenard <90315686+romainbenard@users.noreply.github.com>
2023-12-03 18:08:24 +0100

    Style/single ocd design (#16)
    * style: single ocd view
    
    * style: create about-us and account page placeholder

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,11 @@
+import MainLayout from '@/components/Layout/MainLayout'
+
+const AboutUsPage = () => {
+  return (
+    <MainLayout>
+      <h1>Hello About us</h1>
+    </MainLayout>
+  )
+}
+
+export default AboutUsPage
---
+++ src/app/account/page.tsx
@@ -0,0 +1,11 @@
+import MainLayout from '@/components/Layout/MainLayout'
+
+const AccountPage = () => {
+  return (
+    <MainLayout>
+      <h1>Hello Account</h1>
+    </MainLayout>
+  )
+}
+
+export default AccountPage
