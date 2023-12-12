23b9dde54c4565894e1da2cbe9973dc07533caf2
Romain Benard <romain.benard@swile.co>
2023-12-12 18:34:30 +0100

    feat: create a subcomponent for item

--- src/views/AllOcdsView.tsx
+++ src/views/AllOcdsView.tsx
@@ -3,11 +3,11 @@
 import Link from 'next/link'
 import { Session } from 'next-auth'
 import MainLayout from '@/components/Layout/MainLayout'
+import OcdListItem from '@/components/ocd/OcdListItem'
 import { Ocd } from '@/types/ocd'
 
 type Props = { user: Session['user']; ocds: Ocd[] }
 
-//TODO: Create Component for items
 //TODO: Cursor based pagination + infinite scroll
 //TODO: Add FilterBy (category, date, location)
 
@@ -19,17 +19,7 @@ const AllOcdsView = ({ ocds }: Props) => {
       {ocds.length > 0 ? (
         <ul className="mt-4 flex flex-col gap-3">
           {ocds.map(ocd => (
-            <li
-              key={ocd.id}
-              className="rounded-lg bg-secondary-50 text-secondary-500 p-4"
-            >
-              <Link href={`/ocds/${ocd.id}`}>
-                <p className="text-primary-500 text-xs">
-                  {ocd.date} | {ocd.category.toUpperCase()}
-                </p>
-                <p>{ocd.name}</p>
-              </Link>
-            </li>
+            <OcdListItem key={ocd.id} ocd={ocd} />
           ))}
         </ul>
       ) : (
