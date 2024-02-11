478b489cd1125d293a511196bea2dadef3ca42fb
Romain Benard <romain.benard@swile.co>
2024-02-11 12:41:54 +0100

    refactor: use data file for error

---
+++ src/data/en.ts
@@ -0,0 +1,12 @@
+const enData = {
+  errors: {
+    inputs: {
+      required: 'Required',
+      invalid: 'Invalid value',
+      dateFuture: 'Select today or before',
+    },
+    default: 'An error occurred',
+  },
+}
+
+export default enData
